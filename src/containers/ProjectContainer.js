0e0ce673dc1953d563a2791f81af31ce0559fc42
github username <souzajohnlee@gmail.com>
2020-08-22 14:30:36 -0700

    projects dynamically populate based on amount

--- src/containers/ProjectContainer.js
+++ src/containers/ProjectContainer.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Project from '../components/Project.js'
 
 const ProjectContainer = () => {
+
+    const [projects, setProjects] = useState(["", ""])
+
+    const populateProjects = () => {
+        return projects.map(project => {
+            return <Project />
+        })
+    }
+
     return <div>
         <h2>Projects</h2>
-        <Project />
-        <Project />
+        {projects ? populateProjects() : null}
     </div>
 };
 
