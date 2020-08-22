34828ce943b393bc850f4e51fa89a54f4ea0b864
github username <souzajohnlee@gmail.com>
2020-08-22 14:25:26 -0700

    Added individual projects

---
+++ src/components/Project.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Project = () => {
+    return <div>
+        <h3>Project Name</h3>
+        <p>Project description</p>
+        <p>Project Link</p>
+        <p>Project video demo</p>
+    </div>
+};
+
+export default Project
--- src/containers/ProjectContainer.js
+++ src/containers/ProjectContainer.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Project from '../components/Project.js'
 
 const ProjectContainer = () => {
     return <div>
         <h2>Projects</h2>
+        <Project />
+        <Project />
     </div>
 };
 
