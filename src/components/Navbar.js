7d1fb343520ef138b2248bea213ddf175744f3eb
github username <souzajohnlee@gmail.com>
2020-08-24 21:52:38 -0700

    Added basic navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Navbar = () => {
+    return <div>
+        <ul>
+            <li>Home</li>
+            <li>Resume</li>
+            <li>Blog</li>
+            <li>Github</li>
+            <li>LinkedIn</li>
+            <li>Projects</li>
+            <li>Contact</li>
+        </ul>
+    </div>
+};
+
+export default Navbar
--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ContactInformation from '../components/ContactInformation.js'
 import ProjectContainer from './ProjectContainer.js'
+import Navbar from '../components/Navbar.js'
 
 const HomePage = () => {
     return <div>
+        <Navbar />
         <h1>Home Page</h1>
         <ProjectContainer />
         <ContactInformation />
