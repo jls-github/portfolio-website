8e126e2332ae8b1c6f80fbaa40feba7b844b2998
github username <souzajohnlee@gmail.com>
2020-08-31 10:18:18 -0700

    styling for project header - unfinished

--- src/containers/ProjectContainer.js
+++ src/containers/ProjectContainer.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import Project from '../components/Project.js';
+import CoffeeCodingPicture from '../images/coffee-coding-picture.jpeg';
 
 const ProjectContainer = () => {
 
@@ -12,7 +13,13 @@ const ProjectContainer = () => {
     }
 
     return <div className="projectContainer">
-        <h2>Projects</h2>
+        <div className="project-header">
+            <div className="project-image">
+                <div className="project-image-overlay"></div>
+                <img src={CoffeeCodingPicture} alt=""/>
+            </div>
+            <h2>Projects</h2>
+        </div>
         {projects ? populateProjects() : null}
     </div>
 };
