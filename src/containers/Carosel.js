ff7113d45a8ac1668819e9a8e3a7c222e4a22248
github username <souzajohnlee@gmail.com>
2020-09-08 11:16:05 -0700

    added title and subtitle to carosel

--- src/containers/Carosel.js
+++ src/containers/Carosel.js
@@ -7,12 +7,14 @@ const Carosel = () => {
       backgroundImage: `url(${SeattlePicture})`,
       backgroundPosition: 'center',
       backgroundSize: 'cover',
-      backgroundRepeat: 'no-repeat'
+      backgroundRepeat: 'no-repeat',
     }
 
     return <div className="carosel">
-        <div className="carosel-image" style={ backgroundProperties }>
-          <div className="carosel-image-overlay"></div>
+        <div className="carosel-image" style={ backgroundProperties }></div>
+        <div className="carosel-header">
+          <div className="carosel-title">John Souza</div>
+          <div className="carosel-subtitle">Creating software to build business in the Seattle Metro Area</div>
         </div>
     </div>
 };
