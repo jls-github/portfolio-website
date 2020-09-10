0ae91a2036544145f2f14f3a5ae47b49b9c0630f
github username <souzajohnlee@gmail.com>
2020-09-10 13:24:48 -0700

    added logic to change navbar look depending on what is active

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,17 +4,78 @@ const Navbar = () => {
 
     const [active, setActive] = useState("HOME")
 
+    const onClickHome = e => {
+        setActive("HOME")
+    }
+
+    const onClickProjects = e => {
+        setActive("PROJECTS")
+    }
+
+    const onClickContact = e => {
+        setActive("CONTACT")
+    }
+
+
+    const activeSwitch = () => {
+        switch(active){
+            case("HOME"):
+                return(
+                    <ul className="black-style">
+                        <li className="active-li">Home</li>
+                        <li onClick={(e) => onClickProjects(e)}>Projects</li>
+                        <li onClick={(e) => onClickContact(e)}>Contact</li>
+                        {/* add the following in as buttons
+                        <li>Blog</li>
+                        <li>Github</li>
+                        <li>LinkedIn</li>
+                        <li>Resume</li> */}
+                  </ul>
+                )
+            case("PROJECTS"):
+                return(
+                    <ul className="white-style">
+                        <li onClick={(e) => onClickHome(e)}>Home</li>
+                        <li className="active-li">Projects</li>
+                        <li onClick={(e) => onClickContact(e)}>Contact</li>
+                        {/* add the following in as buttons
+                        <li>Blog</li>
+                        <li>Github</li>
+                        <li>LinkedIn</li>
+                        <li>Resume</li> */}
+                    </ul>
+                )
+            case("CONTACT"):
+                return(
+                    <ul className="black-style">
+                        <li onClick={(e) => onClickHome(e)}>Home</li>
+                        <li onClick={(e) => onClickProjects(e)}>Projects</li>
+                        <li className="active-li">Contact</li>
+                        {/* add the following in as buttons
+                        <li>Blog</li>
+                        <li>Github</li>
+                        <li>LinkedIn</li>
+                        <li>Resume</li> */}
+                      </ul>
+                )
+            default:
+                return(
+                    <ul>
+                        <li>Home</li>
+                        <li>Projects</li>
+                        <li>Contact</li>
+                        {/* add the following in as buttons
+                        <li>Blog</li>
+                        <li>Github</li>
+                        <li>LinkedIn</li>
+                        <li>Resume</li> */}
+                    </ul>
+                )
+        }
+    }
+
     return <div className="navbar">
-        <ul>
-            <li>Home</li>
-            <li>Projects</li>
-            <li>Contact</li>
-            {/* add the following in as buttons
-            <li>Blog</li>
-            <li>Github</li>
-            <li>LinkedIn</li>
-            <li>Resume</li> */}
-        </ul>
+        {activeSwitch()}
     </div>
 };
 
