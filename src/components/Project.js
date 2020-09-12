78a86367672ad3047255887f68d2f6f2bec72ce0
github username <souzajohnlee@gmail.com>
2020-09-11 18:12:08 -0700

    changed styling completely for projects section

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import CodePicture from '../images/code-picture.jpg';
 
 
 const Project = () => {
 
-
-    let backgroundProperties = {
-        backgroundImage: `url(${CodePicture})`,
-        backgroundPosition: 'center',
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-      }
-
     return <div className="project">
-        <div className="project-picture" style={backgroundProperties}></div>
-        <div className="project-picture-overlay"></div>
         <div className="project-card-text">
 
-            <h3>Project Name</h3>
-            <p>Project description</p>
-            <p>Project Link</p>
-            <p>Project video demo</p>
+            <div className="project-title">Project Name</div>
+            <div className="project-description">Project description</div>
+        </div>
+        <div className="project-footer">
+            <div>See More</div>
         </div>
     </div>
 };
