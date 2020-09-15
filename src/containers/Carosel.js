c1304ccb26f51583bb3096aa4789f978007b5d32
github username <souzajohnlee@gmail.com>
2020-09-14 21:37:57 -0700

    playing around wtih carosel

--- src/containers/Carosel.js
+++ src/containers/Carosel.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import SeattlePicture from '../images/SeattlePicture.jpg';
-import CodePicture from '../images/code-picture.jpg'
+import CoffeeCodePicture from '../images/coffee-coding-picture.jpeg'
 
 const Carosel = () => {
 
-    let backgroundProperties = {
-      backgroundImage: `url(${CodePicture})`,
-      backgroundPosition: 'center',
-      backgroundSize: 'cover',
-      backgroundRepeat: 'no-repeat',
-    }
-
     return <div className="carosel">
-        <div className="carosel-image" style={ backgroundProperties }></div>
+      <div className="carosel-image-wrapper">
+        <img src={ CoffeeCodePicture } alt="" className="carosel-image"></img>
+
+      </div>
         <div className="carosel-header">
           <div className="carosel-title">John Souza</div>
           <div className="carosel-subtitle">Creating software to build business in the Seattle Metro Area</div>
