4379822cc54321b020369c8264d8f57867dff90d
github username <souzajohnlee@gmail.com>
2020-09-17 16:43:54 -0700

    wrap projects for positioning

--- src/containers/ProjectContainer.js
+++ src/containers/ProjectContainer.js
@@ -27,30 +27,33 @@ const ProjectContainer = () => {
 
 
     return <div className="projectContainer">
-        <div className="project-background-left">
-            <div className="project-section-picture" style={ backgroundProperties(CoffeePicture)}></div>
-            <div className="project-header">
-                <div className="project-header-text">
-                    <h2>See My Work</h2>
-                </div>
-            </div>
-            <div className="projectFlexBoxWrapper">
-                <div className="projectFlexBox">
-                    {projects ? populateProjects() : null}
+        <div className="projectWrapper">
 
+            <div className="project-background-left">
+                <div className="project-section-picture" style={ backgroundProperties(CoffeePicture)}></div>
+                <div className="project-header">
+                    <div className="project-header-text">
+                        <h2>See My Work</h2>
+                    </div>
                 </div>
-            </div>
+                <div className="projectFlexBoxWrapper">
+                    <div className="projectFlexBox">
+                        {projects ? populateProjects() : null}
 
-        </div>
-        <div className="project-background-right-top">
-            <div className="project-section-picture" style={ backgroundProperties(CodePicture)}></div>
-            Maybe put one project here
-            Or a tech stack section
-        </div>
-        <div className="project-background-right-bottom">
-            <div className="project-section-picture" style={ backgroundProperties(SeattlePicture)}></div>
-            And another here?
-            Or an About Me Section
+                    </div>
+                </div>
+
+            </div>
+            <div className="project-background-right-top">
+                <div className="project-section-picture" style={ backgroundProperties(CodePicture)}></div>
+                Maybe put one project here
+                Or a tech stack section
+            </div>
+            <div className="project-background-right-bottom">
+                <div className="project-section-picture" style={ backgroundProperties(SeattlePicture)}></div>
+                And another here?
+                Or an About Me Section
+            </div>
         </div>
 
     </div>
