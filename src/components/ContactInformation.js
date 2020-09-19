31fd78187c78246c51b50698aed16583644a34c1
github username <souzajohnlee@gmail.com>
2020-09-19 12:27:15 -0700

    center contact form

--- src/components/ContactInformation.js
+++ src/components/ContactInformation.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ContactForm from './ContactForm.js';
 
 const ContactInformation = () => {
-    return <div>
-        <h2>Contact me!</h2>
-        <div>Email: ---</div>
-        <div>Phone: ---</div>
-        <ContactForm />
+    return <div className="contact-wrapper">
+        <div className="contact-container">
+
+            <h2>Contact me!</h2>
+            <div>Email: ---</div>
+            <div>Phone: ---</div>
+            <ContactForm />
+        </div>
     </div>
 };
 
