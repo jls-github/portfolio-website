f606859e798af15ed53597269b559f5968a9867c
github username <souzajohnlee@gmail.com>
2020-09-19 12:08:14 -0700

    import contact form

--- src/components/ContactInformation.js
+++ src/components/ContactInformation.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import ContactForm from 'ContactForm.js';
 
 const ContactInformation = () => {
     return <div>
         <h2>Contact me!</h2>
         <div>Email: ---</div>
         <div>Phone: ---</div>
+        <ContactForm />
     </div>
 };
 
