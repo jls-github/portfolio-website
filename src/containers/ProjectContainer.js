a0e24900c83840e5d4572e2c6360c3f9d3626aef
github username <souzajohnlee@gmail.com>
2020-09-21 21:57:21 -0700

    simplify project collection

--- src/containers/ProjectContainer.js
+++ src/containers/ProjectContainer.js
@@ -1,10 +1,5 @@
 import React, {useState} from 'react';
 import Project from '../components/Project.js';
-import CoffeeCodingPicture from '../images/coffee-coding-picture.jpeg';
-import CodePicture from '../images/code-picture.jpg';
-import SeattlePicture from '../images/SeattlePicture.jpg';
-import CoffeePicture from '../images/CoffeePicture.jpg';
-
 
 const ProjectContainer = () => {
 
@@ -16,45 +11,11 @@ const ProjectContainer = () => {
         })
     }
 
-    const backgroundProperties = (picture) => {
-        return {       
-            backgroundImage: `url(${picture})`,
-            backgroundPosition: 'center',
-            backgroundSize: 'cover',
-            backgroundRepeat: 'no-repeat',
-        }
-      }
 
 
-    return <div className="framed-wrapper">
-        <div className="wrapper projectWrapper">
-
-            <div className="project-background-left">
-                <div className="project-section-picture" style={ backgroundProperties(CoffeePicture)}></div>
-                <div className="project-header">
-                    <div className="project-header-text">
-                        <h2>See My Work</h2>
-                    </div>
-                </div>
-                <div className="projectFlexBoxWrapper">
-                    <div className="projectFlexBox">
-                        {projects ? populateProjects() : null}
-
-                    </div>
-                </div>
-
-            </div>
-            <div className="project-background-right-top">
-                <div className="project-section-picture" style={ backgroundProperties(CodePicture)}></div>
-                Maybe put one project here
-                Or a tech stack section
-            </div>
-            <div className="project-background-right-bottom">
-                <div className="project-section-picture" style={ backgroundProperties(SeattlePicture)}></div>
-                And another here?
-                Or an About Me Section
-            </div>
-        </div>
+    return <div className="framed-wrapper project-wrapper">
+        <h2>My Work</h2>
+        {populateProjects()}
 
     </div>
 };
