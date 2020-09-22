132d1d65142c9c6047a2fe0b8ca31c0815d69e47
github username <souzajohnlee@gmail.com>
2020-09-21 22:04:18 -0700

    remove useless contact info

--- src/components/ContactInformation.js
+++ src/components/ContactInformation.js
@@ -4,10 +4,6 @@ import ContactForm from './ContactForm.js';
 const ContactInformation = () => {
     return <div className="contact-wrapper">
         <div className="contact-container">
-
-            <h2>Contact me!</h2>
-            <div>Email: ---</div>
-            <div>Phone: ---</div>
             <ContactForm />
         </div>
     </div>
