c91f22e0101690d4ed70f462ac6a35ac3cd07123
github username <souzajohnlee@gmail.com>
2020-09-22 21:44:07 -0700

    remove text from projects

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,11 +4,7 @@ import React from 'react';
 const Project = ({side}) => {
 
     return <div className="project">
-        <div className="project-card-text">
-
-            <div className="project-title">Soundside Mobile</div>
-            <div className="project-description">Mobile web application built for a local church in Tacoma, WA</div>
-        </div>
+        <div></div>
         <div className="project-footer">
             <div>See More</div>
         </div>
