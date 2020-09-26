428e9f408efe5d8cfd7a7f2e019bf5989756f2f0
github username <souzajohnlee@gmail.com>
2020-09-25 20:31:09 -0700

    remove bug with x display

--- src/components/Project.js
+++ src/components/Project.js
@@ -8,7 +8,7 @@ const Project = ({title, picture}) => {
             <div className="project-card-front">
                 <div className="wrapper">
                     <img src={picture} alt=""/>
-                </div>x`
+                </div>
                 <div className="project-footer">
                     <div>{title}</div>
                 </div>
