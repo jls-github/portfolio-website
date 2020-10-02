b413017acb506fb843643c142bdbe389e7c89c9e
github username <souzajohnlee@gmail.com>
2020-10-01 18:10:34 -0700

    set up grid for page

--- src/containers/Carosel.js
+++ src/containers/Carosel.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import PortfolioPicture from '../images/John_Souza.jpeg'
+import WallPicture from '../images/wall.jpg';
+import PaperTexture from '../images/paper_texture_5.jpg';
 
 const Carosel = () => {
 
 
-    return <div className="framed-wrapper">
-        <div className="carosel-header">
-          <div className="carosel-title">John Souza</div>
-          <div className="carosel-subtitle">Creating software to build business in the Seattle Metro Area</div>
+    return <div className="carosel-container">
+
+        <img className="background-picture" src={WallPicture} alt=""/>
+        <div className="frame">
+            <img className="paper-texture-image" src={PaperTexture} alt=""/>
+          <div className="framed-wrapper">
+              <div className="carosel-header">
+                <div className="carosel-title">John Souza</div>
+                <div className="carosel-subtitle">Creating software to build business in the Seattle Metro Area</div>
+              </div>
+              <div class="carosel-image"><img src={PortfolioPicture} alt=""/></div>
+          </div>
         </div>
-        <div class="carosel-image"><img src={PortfolioPicture} alt=""/></div>
-    </div>
+      </div>
+    
 };
 
 export default Carosel;
--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -3,8 +3,7 @@ import ContactInformation from '../components/ContactInformation.js';
 import ProjectContainer from './ProjectContainer.js';
 import Navbar from '../components/Navbar.js';
 import Carosel from './Carosel.js';
-import WallPicture from '../images/wall.jpg';
-import PaperTexture from '../images/paper_texture_5.jpg';
+
 
 const HomePage = () => {
 
@@ -29,13 +28,10 @@ const HomePage = () => {
     
     return (
     <div className="main">
-        <img className="background-picture" src={WallPicture} alt=""/>
         <Navbar activePage={activePage} onSetActive={(page) => onSetActive(page)}/>
-        <div className="frame">
-            <img className="paper-texture-image" src={PaperTexture} alt=""/>
-
-            {displayPage()}
-        </div>
+        <Carosel />
+        <ProjectContainer />
+        <ContactInformation />
     </div>
     )
 };
