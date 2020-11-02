8c325ab023af40304a4ee873608190db1d26c345
github username <souzajohnlee@gmail.com>
2020-11-01 19:49:58 -0800

    abstract project data out of container

---
+++ src/data/ProjectData.js
@@ -0,0 +1,11 @@
+import CoffeePicture from '../images/CoffeePicture.jpg'
+import SoundsideIcon from '../images/Soundside_Mark.png'
+import DwellinglyIcon from '../images/Dwellingly_Logo.png'
+
+const ProjectData = [
+{title: "Soundside Mobile", frontPicture: CoffeePicture, backPicture: SoundsideIcon,description: "Digital worship guide and connection card allowing Soundside Church in Tacoma, WA to minimize COVID-19 spread", liveSite: "https://soundsidechurch.netlify.app", github: "https://github.com/jls-github/flatchat_frontend"},
+{title: "Flatchat", frontPicture: CoffeePicture, backPicture: DwellinglyIcon,description: "Real time chat application allowing multiple users to join a chat room and send messages without page reload", liveSite: "https://ecstatic-swartz-aca3e4.netlify.app/", github: "https://github.com/jls-github/flatchat_frontend"}, 
+{title: "Dwellingly", frontPicture: CoffeePicture, backPicture: CoffeePicture,description: "Canvas-like Ruby on Rails application that facilitates lesson management and test-taking for teachers and students.", liveSite: "", github: "https://github.com/jls-github/school_central"}
+]
+
+export default ProjectData
