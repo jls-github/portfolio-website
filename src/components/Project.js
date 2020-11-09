23fe67cdccd9146f3c66e62db06a1cb6eb3a270c
github username <souzajohnlee@gmail.com>
2020-11-08 23:22:35 -0800

    create basic html for project pages

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const Project = ({title, frontPicture, backPicture, description, liveSite, github}) => {
-    return <div>Project</div>
+    return (
+        <div>
+            <div>Title: {title}</div>
+            <div>Description: {description}</div>
+            <div>Live Site: {liveSite}</div>
+            <div>Github: {github}</div>
+        </div>
+    )
 }
 
 export default Project
